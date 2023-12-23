5a0a4fe618c6900f5db9b0b2528092f0fe33abcb
Меркин Арсен <arsen.merkin@gmail.com>
2023-12-23 05:51:49 +0500

    added routing to [id]

--- src/pages/manager/orders/[id].vue
+++ src/pages/manager/orders/[id].vue
@@ -10,7 +10,7 @@
       <template #default>
         <div v-if="orders" class="flex column gap-base">
           <StatusCard v-for="order in orders" :key="order.id">
-            <p class="text-20 default-inner-gap"><NuxtLink :to="`/manager/orders/${order.id}`" >№: {{ order.id }}</NuxtLink></p>
+            <p class="text-20 default-inner-gap">№: {{ order.id }}</p>
           </StatusCard>
         </div>
         <Empty v-else>Заказов еще нет</Empty>
@@ -33,11 +33,12 @@ import type { QueryParams, Order } from "./types";
 import type { YearParams } from "./components/YearPicker";
 import type { SearchParams } from "./components/SearchPanel";
 
+const route = useRoute()
 const router = useRouter()
 
 const queryParams: Ref<QueryParams> = ref({
-  search_value: "",
-  search_type: "",
+  search_value: `${route.params.id}`,
+  search_type: "order_number",
   year: "",
   date_start: "",
   date_finish: ""
@@ -53,16 +54,16 @@ const updateParams = (current: any, updates: YearParams | SearchParams) => {
   return { ...current, ...updates };
 };
 
-const fetchOrders = async () => {
-  const data: string = await internalAPIFetch("method/orders.getTest", { params: queryParams.value });
+const fetchOrder = async () => {
+  const data: string = await internalAPIFetch(`method/orders.getTest/`, { params: queryParams.value });
   const { response } = JSON.parse(data);
   orders.value = response.data.orders;
 };
 
-fetchOrders()
+fetchOrder()
 
 watch(queryParams, () => {
-  fetchOrders()
+  fetchOrder()
 });
 </script>
 
