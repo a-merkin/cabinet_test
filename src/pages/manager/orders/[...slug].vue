ab39d5e545eb6e00b4b6d7367de296ddc0e414bf
Меркин Арсен <arsen.merkin@gmail.com>
2023-12-24 19:51:38 +0500

    added loading state for fetching orders

--- src/pages/manager/orders/[...slug].vue
+++ src/pages/manager/orders/[...slug].vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <Orders>
-      <div v-if="orders" class="flex column gap-base">
+      <div v-if="orders || loading" class="flex column gap-base">
         <StatusCard v-for="order in orders" :key="order.id">
           <p class="text-20 default-inner-gap">
             <NuxtLink :to="`/manager/orders/${order.id}`">№: {{ order.id }}</NuxtLink>
@@ -27,10 +27,14 @@ const route = useRoute();
 
 const orders: Ref<Order[] | null> = ref(null);
 
+const loading: Ref<boolean> = ref(false)
+
 const fetchOrders = async () => {
+  loading.value = true
   const data: string = await internalAPIFetch(`method/orders.getTest/${route.params.slug}`, { params: route.query as QueryParams });
   const { response } = JSON.parse(data);
   orders.value = response.data.orders;
+  loading.value = false
 };
 
 fetchOrders();
